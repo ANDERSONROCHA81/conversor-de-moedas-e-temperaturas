eabff2f40c00db3a509726129785eeec70d4c610
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2023-07-13 16:19:10 -0300

    Adição do método para validar a entrada do usuário na classe Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         Object[] opcoes = {"Conversor de Moedas", "Conversor de Temperaturas"};
+        String entrada;
         String escolha = (String) JOptionPane.showInputDialog(null, "Escolha o tipo de conversor", "Menu",
                 JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
 
-        JOptionPane.showMessageDialog(null, escolha);
+        if (escolha.equals("Conversor de Moedas")){
+            entrada = JOptionPane.showInputDialog(null, "Insira um valor", "Input",
+                    JOptionPane.QUESTION_MESSAGE);
+            validaEntrada(entrada);
+        }
+    }
+
+    public static void validaEntrada(String entrada){
+        double valor;
+        if (entrada.matches("[0-9]*")){
+            valor = Double.parseDouble(entrada);
+
+        }else {
+            JOptionPane.showMessageDialog(null, "A entrada deve ser apenas números");
+        }
     }
 }
