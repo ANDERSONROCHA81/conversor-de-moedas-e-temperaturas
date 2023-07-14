7fde8d747c0832004299e7a6944d67b658e0a2c6
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2023-07-14 19:28:20 -0300

    Mudança da estrutura if else para a estrutura switch case nos métodos de conversão de moedas

--- src/main/java/ConversorDeMoedas.java
+++ src/main/java/ConversorDeMoedas.java
@@ -14,22 +14,27 @@ public class ConversorDeMoedas {
         String escolha = (String) JOptionPane.showInputDialog(null, "Escolha para qual moeda deseja converter", "Moedas",
                 JOptionPane.INFORMATION_MESSAGE, null, conversoes, conversoes[0]);
 
-        if (escolha.equals("Real para Dólar")) {
-            resultado = valor / cotacaoDolar;
-            String mensagem = String.format("Agora você tem U$ %.2f dolares", resultado);
-            JOptionPane.showMessageDialog(null, mensagem);
-        }else if (escolha.equals("Real para Euro")) {
-            resultado = valor / cotacaoEuro;
-            String mensagem = String.format("Agora você tem € %.2f euros", resultado);
-            JOptionPane.showMessageDialog(null, mensagem);
-        }else if (escolha.equals("Real para Libra Esterlina")) {
-            resultado = valor / cotacaoLibraEsterlina;
-            String mensagem = String.format("Agora você tem £ %.2f libras esterlinas", resultado);
-            JOptionPane.showMessageDialog(null, mensagem);
-        }else if (escolha.equals("Real para Peso Argentino")) {
-            resultado = valor / cotacaoPesoArgentino;
-            String mensagem = String.format("Agora você tem $ %.2f pesos argentinos", resultado);
-            JOptionPane.showMessageDialog(null, mensagem);
+        switch (escolha) {
+            case "Real para Dólar" -> {
+                resultado = valor / cotacaoDolar;
+                String mensagem = String.format("Agora você tem U$ %.2f dolares", resultado);
+                JOptionPane.showMessageDialog(null, mensagem);
+            }
+            case "Real para Euro" -> {
+                resultado = valor / cotacaoEuro;
+                String mensagem = String.format("Agora você tem € %.2f euros", resultado);
+                JOptionPane.showMessageDialog(null, mensagem);
+            }
+            case "Real para Libra Esterlina" -> {
+                resultado = valor / cotacaoLibraEsterlina;
+                String mensagem = String.format("Agora você tem £ %.2f libras esterlinas", resultado);
+                JOptionPane.showMessageDialog(null, mensagem);
+            }
+            case "Real para Peso Argentino" -> {
+                resultado = valor / cotacaoPesoArgentino;
+                String mensagem = String.format("Agora você tem $ %.2f pesos argentinos", resultado);
+                JOptionPane.showMessageDialog(null, mensagem);
+            }
         }
     }
 }
